4f28847774ccc84d835398ab72306beb8f2abbec
Andrey Kumykov <aakumykov@yandex.ru>
2022-10-27 11:53:56 +0300

    NullPointerException вместо IllegalArgumentException.

--- argument_utils/src/main/java/com/github/aakumykov/argument_utils/ArgumentUtils.java
+++ argument_utils/src/main/java/com/github/aakumykov/argument_utils/ArgumentUtils.java
@@ -4,22 +4,22 @@ import androidx.annotation.Nullable;
 
 public class ArgumentUtils {
 
-    public static <T> T checkNotNull(T arg) {
+    public static <T> T checkNotNull(T arg) throws NullPointerException {
         if (null == arg)
-            throw new IllegalArgumentException("Argument cannot be null");
+            throw new NullPointerException("Argument cannot be null");
         return arg;
     }
 
     public static void assertBothNullOrNot(@Nullable Object firstArg,
                                            @Nullable Object secondArg)
-            throws IllegalArgumentException
+            throws NullPointerException
     {
 
         int firstArgNum = (null == firstArg) ? 0 : 1;
         int secondArgNum = (null == secondArg) ? 0 : 1;
 
         if (1 == (firstArgNum + secondArgNum))
-            throw new IllegalArgumentException("Both arguments must be null or not. " +
+            throw new NullPointerException("Both arguments must be null or not. " +
                     "Now first is : " + ((null == firstArg)?"null":"not null") +
                     ", second is " + ((null == secondArg)?"null":"not null")
             );
